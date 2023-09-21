4ebbf6542bf8d64442ea1e4587984159c7a201ac
forwward <joaosalessilva08@gmail.com>
2023-09-21 19:53:37 -0300

    Atividade 1

---
+++ atv1.py
@@ -0,0 +1,16 @@
+# Atividade 1
+nome = "João Vitor" 
+sobrenome = "Sales da Silva"
+idade = 2023 - 2004
+altura = "1,75 metros"
+peso = (100 - 27)
+print("Nome: ",nome)
+
+print("Sobrenome: ",sobrenome)
+print("Idade: ",idade)
+print("Altura: ",altura)
+print("Peso: ",peso,'KG')
+print("É maior de idade.")
+
+
+
