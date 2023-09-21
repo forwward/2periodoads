30d4f1c592239bdd99bb38cab7068582192e06c4
forwward <joaosalessilva08@gmail.com>
2023-09-21 20:34:14 -0300

    Atividade 3

---
+++ atv3.py
@@ -0,0 +1,10 @@
+# Atividade 3
+valor1 = int(input("Digite um valor."))
+valor2 = int(input("Digite um outro valor."))
+if valor1 > valor2:
+    print("O primeiro valor é maior.")
+elif valor2 > valor1:
+    print("O segundo valor é maior.")
+elif valor1 == valor2:
+    print("Os valores são iguais.")
+    
