ade3265ad08d552b81eac7f3a9746209904d6b73
Forwward <joaosalessilva08@gmail.com>
2023-10-06 20:20:26 -0300

    06outubro

--- 0610.py
+++ 0610.py
@@ -1,6 +1,14 @@
 class Carro:
  pneus = 4
 
-def _init_(self, marca, modelo):
+def  _init_(self, marca, modelo):
     self.marca = marca
     self.modelo = modelo
+
+carro1 = Carro("Ford", "Fiesta")
+carro2 = Carro("Honda", "Civic")
+
+print(carro1.pneus)
+print(carro2.pneus)
+
+print (Carro.pneus)
