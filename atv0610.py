d5eac83d9bedfce5aabd952d2fca7d590cd1be6b
Forwward <joaosalessilva08@gmail.com>
2023-10-06 21:14:11 -0300

    atividade 0610

--- atv0610.py
+++ atv0610.py
@@ -1,10 +1,14 @@
 class Estudante:
-    def _init_(self, nome, idade):
+    def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
     
     def Apresentar(self):
-        print(f("Meu nome é {self.nome} e minha idade é {self.idade}."))
+        print(f"Meu nome é {self.nome} e minha idade é {self.idade}.")
+
 
 aluno1 = Estudante("João", 18)
+aluno1.Apresentar()
+
+
 
